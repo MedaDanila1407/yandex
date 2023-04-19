bea67dff022656a3819886edeb6507003ed41c72
MedaDanila <medanila00@gmail.com>
2023-04-19 18:12:10 +0300

    Updates

--- src/services/reducers/rootReducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { burgerConstructorReducer } from './burgerConstructorReducer';
-import { userReducer } from './userReducer';
-import { wsReducer } from './wsReducer';
-
-export const rootReducer = combineReducers({ burgerConstructorReducer, userReducer, wsReducer });
